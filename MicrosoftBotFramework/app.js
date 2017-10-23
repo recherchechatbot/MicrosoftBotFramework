4a7f3f92ecadd0f0625d901d35c0af8a6b8a77ac
a.rousselot <a.rousselot@DT-130301>
2017-10-23 10:41:42 +0200

    q

--- MicrosoftBotFramework/app.js
+++ MicrosoftBotFramework/app.js
@@ -29,8 +29,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 //Creation chat connector pour communiquer avec le serve bot framework
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: MICROSOFT_APP_ID,
+    appPassword: MICROSOFT_APP_PASSWORD
 });
 
 
