f582ce2480f6c60d958a921eb33c56f0f72ba6e1
a.rousselot <a.rousselot@DT-130301>
2017-10-24 16:04:14 +0200

    d

--- MicrosoftBotFramework/app.js
+++ MicrosoftBotFramework/app.js
@@ -185,7 +185,7 @@ bot.dialog('login', [
         console.log("Mot de passe: " + session.dialogData.mdp);
         getIdrc(session.dialogData.email, session.dialogData.mdp, session)
             .then(() => console.log("voyons voir si l'idrc est accessible en dehors de la fonction getidrc: " + session.dialogData.idrc))
-            .then(() =>getToken(session.dialogData.email, session.dialogData.mdr, session.dialogData.idrc, session))
+            .then(() =>getToken(session.dialogData.email, session.dialogData.mdp, session.dialogData.idrc, session))
             .then(() =>getSessionId(session.dialogData.email, session.dialogData.mdp,session))
             .then(() =>session.send("Vous êtes bien connecté"))
 
