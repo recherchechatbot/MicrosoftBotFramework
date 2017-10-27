1fbf9f7fd99bf5cbedd7d118577b94c8f521690f
a.rousselot <a.rousselot@DT-130301>
2017-10-27 15:49:54 +0200

    s

--- MicrosoftBotFramework/app.js
+++ MicrosoftBotFramework/app.js
@@ -1,5 +1,4 @@
-
-﻿'use strict';
+'use strict';
 var debug = require('debug');
 var express = require('express');
 var path = require('path');
@@ -16,14 +15,10 @@ const URL_MCO = process.env.URL_MCO;
 const URL_RC = process.env.URL_RC;
 const MSQ_JETON_APP_RC = process.env.MSQ_JETON_APP_RC;
 const MSQ_APP_RC = process.env.MSQ_APP_RC;
-const faq = require('./faq.js');
-
-
 var server = restify.createServer();
 var app = express();
 const MICROSOFT_APP_ID = process.env.MICROSOFT_APP_ID; 
 const MICROSOFT_APP_PASSWORD = process.env.MICROSOFT_APP_PASSWORD;
-
 const LUIS_APP_URL = process.env.LUIS_APP_URL;
 
 //Setup server restify
@@ -38,19 +33,14 @@ var connector = new builder.ChatConnector({
 });
 
 
-
-
 //listen messages utlisateurs
 server.post('/api/messages', connector.listen());
-
 
 
 //Reception message utilisateur
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("SALUT SALUT");
 });
-
-
 
 
 //Ajout reconnaissance LUIS
@@ -74,8 +64,6 @@ bot.recognizer(recognizer);
 //intents.matches('Localisation', function (session, args) {
 //    session.send("c'est exact")
 //})
-
-
 
 
 function parseCookies(cookiesString) {
@@ -425,359 +413,28 @@ bot.dialog('getrecette', [
 });
 
 
-//bot.dialog('adaptive', [
-//    function (session) {
-//        console.log('je suis dans le dialogue adaptive card ');
-//        var card = {
-//            "type": "AdaptiveCard",
-//            "version": "1.0",
-//            "body": [
-//                {
-//                    "type": "ColumnSet",
-//                    "columns": [
-//                        {
-//                            "type": "Column",
-//                            "width": 2,
-//                            "items": [
-//                                {
-//                                    "type": "TextBlock",
-//                                    "text": "Connexion",
-//                                    "weight": "bolder",
-//                                    "size": "medium"
-//                                },
-//                                {
-//                                    "type": "TextBlock",
-//                                    "text": "Ton e-mail :",
-//                                    "wrap": true
-//                                },
-//                                {
-//                                    "type": "Input.Text",
-//                                    "id": "email",
-//                                    "placeholder": "moi@exemple.com"
-//                                },
-//                                {
-//                                    "type": "TextBlock",
-//                                    "text": "Ton mot de passe :",
-//                                    "wrap": true
-//                                },
-//                                {
-//                                    "type": "Input.Text",
-//                                    "id": "mdp",
-//                                    "placeholder": "•••••••"
-//                                }
-//                            ]
-//                        }
-//                    ]
-//                }
-//            ],
-//            "actions": [
-//                {
-//                    "type": "Action.Submit",
-//                    "title": "En avant!",
-//                    "data": {
-//                        'type':'login'
+//<<<<<<<<<<<<<<<<<<<<<<FAQ>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-//                    }
-//                }
-//            ]
-//}
-    
-
-        
-//        session.send(new builder.Message(session).addAttachment({
-//            contentType: "application/vnd.microsoft.card.adaptive",
-//            content: card
-//        }));
-//        if (session.message && session.message.value) {
-//            // A Card's Submit Action obj was received
-//            console.log("ceci est inchallah la data utilisateur: "+ JSON.stringify(session.message.value));
-//        }
-//    }
-//]).triggerAction({
-//    matches: /^adaptive$/i,
-//    });
-
-//AdaptiveCards.setHostConfig({
-//    "spacing": {
-//        "small": 3,
-//        "default": 8,
-//        "medium": 20,
-//        "large": 30,
-//        "extraLarge": 40,
-//        "padding": 20
-//    },
-//    "separator": {
-//        "lineThickness": 1,
-//        "lineColor": "#EEEEEE"
-//    },
-//    "supportsInteractivity": true,
-//    "fontFamily": "Segoe UI",
-//    "fontSizes": {
-//        "small": 12,
-//        "default": 14,
-//        "medium": 17,
-//        "large": 21,
-//        "extraLarge": 26
-//    },
-//    "fontWeights": {
-//        "lighter": 200,
-//        "default": 400,
-//        "bolder": 600
-//    },
-//    "containerStyles": {
-//        "default": {
-//            "backgroundColor": "#00000000",
-//            "fontColors": {
-//                "default": {
-//                    "normal": "#333333",
-//                    "subtle": "#EE333333"
-//                },
-//                "accent": {
-//                    "normal": "#2E89FC",
-//                    "subtle": "#882E89FC"
-//                },
-//                "attention": {
-//                    "normal": "#cc3300",
-//                    "subtle": "#DDcc3300"
-//                },
-//                "good": {
-//                    "normal": "#54a254",
-//                    "subtle": "#DD54a254"
-//                },
-//                "warning": {
-//                    "normal": "#e69500",
-//                    "subtle": "#DDe69500"
-//                }
-//            }
-//        },
-//        "emphasis": {
-//            "backgroundColor": "#08000000",
-//            "fontColors": {
-//                "default": {
-//                    "normal": "#333333",
-//                    "subtle": "#EE333333"
-//                },
-//                "accent": {
-//                    "normal": "#2E89FC",
-//                    "subtle": "#882E89FC"
-//                },
-//                "attention": {
-//                    "normal": "#cc3300",
-//                    "subtle": "#DDcc3300"
-//                },
-//                "good": {
-//                    "normal": "#54a254",
-//                    "subtle": "#DD54a254"
-//                },
-//                "warning": {
-//                    "normal": "#e69500",
-//                    "subtle": "#DDe69500"
-//                }
-//            }
-//        }
-//    },
-//    "imageSizes": {
-//        "small": 40,
-//        "medium": 80,
-//        "large": 160
-//    },
-//    "actions": {
-//        "maxActions": 5,
-//        "spacing": 2,
-//        "buttonSpacing": 10,
-//        "showCard": {
-//            "actionMode": 0,
-//            "inlineTopMargin": 16
-//        },
-//        "actionsOrientation": 0,
-//        "actionAlignment": 3
-//    },
-//    "adaptiveCard": {
-//        "allowCustomStyle": false
-//    },
-//    "image": {
-//        "size": 3
-//    },
-//    "imageSet": {
-//        "imageSize": 3,
-//        "maxImageHeight": 100
-//    },
-//    "factSet": {
-//        "title": {
-//            "color": 0,
-//            "size": 1,
-//            "isSubtle": false,
-//            "weight": 2,
-//            "wrap": true,
-//            "maxWidth": 150
-//        },
-//        "value": {
-//            "color": 0,
-//            "size": 1,
-//            "isSubtle": false,
-//            "weight": 1,
-//            "wrap": true
-//        },
-//        "spacing": 10
-//    }
-//})
-
-bot.dialog('adaptive2', [
+bot.dialog('ajoutexpress', [
     function (session) {
-        console.log('je suis dans le dialogue adaptive card ');
-        var card = {
-            
-	"$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-            "type": "AdaptiveCard",
-            "version": "1.0",
-            "body": [
+        let msg = {
+            "type": "message",
+            "text": "Vous êtes pressé ? L’Ajout Express vous permet d’ajouter des produits à votre panier en seulement quelques clics.Rien de plus simple, sélectionnez cette option lorsque vous êtes dans votre panier sur le site de courses en ligne.En cliquant sur le bouton ci- dessous, vous accédez directement aux rayons puis aux sous- familles.Vous n’avez plus qu’à compléter votre panier.",
+            "attachments": [
                 {
-                    "type": "Container",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "Publish Adaptive Card schema",
-                            "weight": "bolder",
-                            "size": "medium"
-                        },
-                        {
-                            "type": "ColumnSet",
-                            "columns": [
-                                {
-                                    "type": "Column",
-                                    "width": "auto",
-                                    "items": [
-                                        {
-                                            "type": "Image",
-                                            "url": "https://pbs.twimg.com/profile_images/3647943215/d7f12830b3c17a5a9e4afcc370e3a37e_400x400.jpeg",
-                                            "size": "small",
-                                            "style": "person"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "type": "Column",
-                                    "width": "stretch",
-                                    "items": [
-                                        {
-                                            "type": "TextBlock",
-                                            "text": "Matt Hidinger",
-                                            "weight": "bolder",
-                                            "wrap": true
-                                        },
-                                        {
-                                            "type": "TextBlock",
-                                            "spacing": "none",
-                                            "text": "Created {{DATE(2017-02-14T06:08:39Z,Short)}}",
-                                            "isSubtle": true,
-                                            "wrap": true
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "type": "Container",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "Now that we have defined the main rules and features of the format, we need to produce a schema and publish it to GitHub. The schema will be the starting point of our reference documentation.",
-                            "wrap": true
-                        },
-                        {
-                            "type": "FactSet",
-                            "facts": [
-                                {
-                                    "title": "Board:",
-                                    "value": "Adaptive Card"
-                                },
-                                {
-                                    "title": "List:",
-                                    "value": "Backlog"
-                                },
-                                {
-                                    "title": "Assigned to:",
-                                    "value": "Matt Hidinger"
-                                },
-                                {
-                                    "title": "Due date:",
-                                    "value": "Not set"
-                                }
-                            ]
-                        }
-                    ]
+                    "contentType": "image/png",
+                    "contentUrl": "https://img4.hostingpics.net/pics/782644Capture.png",
+                    "name": "Ajout Express"
                 }
             ],
-            "actions": [
-                {
-                    "type": "Action.ShowCard",
-                    "title": "Set due date",
-                    "card": {
-                        "type": "AdaptiveCard",
-                        "body": [
-                            {
-                                "type": "Input.Date",
-                                "id": "dueDate",
-                                "title": "Select due date"
-                            }
-                        ],
-                        "actions": [
-                            {
-                                "type": "Action.Submit",
-                                "title": "OK"
-                            }
-                        ]
-                    }
-                },
-                {
-                    "type": "Action.ShowCard",
-                    "title": "Comment",
-                    "card": {
-                        "type": "AdaptiveCard",
-                        "body": [
-                            {
-                                "type": "Input.Text",
-                                "id": "comment",
-                                "isMultiline": true,
-                                "placeholder": "Enter your comment"
-                            }
-                        ],
-                        "actions": [
-                            {
-                                "type": "Action.Submit",
-                                "title": "OK"
-                            }
-                        ]
-                    }
-                },
-                {
-                    "type": "Action.OpenUrl",
-                    "title": "View",
-                    "url": "http://adaptivecards.io"
-                }
-            ]
-        }
-        
-        var msg = new builder.Message(session);
-        msg.addAttachment(card);
+
+        };
         session.send(msg);
+
     }
 ]).triggerAction({
-    matches: /^adaptive2$/i,
+    matches: 'FAQ.Ajout.Express',
 });
-////creation dialogue recette
-//bot.dialog('RechercheRecette', [
-//    //mon code (appel webservice, format de réponse etc...
-//    function (session, args, next) {
-//        session.send('bonjour, vous avez tapé : %s', session.message.text);
-//        console.log("on est dans le dialogue recettes");
-//        var intent = args.intent;
-//        var produit = builder.EntityRecogniser.findEntity(intent.entities, 'Nourriture');
-//        builder.Prompts.text(session, 'Vous recherchez des recettes à base de ' + produit);
-        
-//    }
-//]).triggerAction({ matches: 'Recettes' });//Dialogue lancé ssi l'intent matché est "recettes"
+
 
 
